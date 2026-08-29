bdd453cbaf477ab7d3f775aed14a22ebeaa2e600
b0b0aung <160343524+boboaung1337@users.noreply.github.com>
2026-08-29 15:56:27 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download PetitPotam.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/PetitPotam.py && sudo mv PetitPotam.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/PetitPotam.py
+
+# Download getnthash.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/getnthash.py && sudo mv getnthash.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/getnthash.py
+
+# Download gets4uticket.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/gets4uticket.py && sudo mv gets4uticket.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/gets4uticket.py
+
+# Download gettgtpkinit.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/gettgtpkinit.py && sudo mv gettgtpkinit.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/gettgtpkinit.py
+
+# Download passthecert.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/passthecert.py && sudo mv passthecert.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/passthecert.py
+
+echo "All tools installed successfully!"
