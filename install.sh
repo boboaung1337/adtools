81459868b741593b2970330f1b1c35f8aeb7e41c
b0b0aung <160343524+boboaung1337@users.noreply.github.com>
2026-09-05 23:59:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,4 +18,77 @@ wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/passthecert.py 
 # Download ntlm_theft.py
 wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/ntlm_theft.py && sudo mv ntlm_theft.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/ntlm_theft.py
 
+# Download keepass4brute
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/keepass4brute && sudo mv keepass4brute /usr/local/bin/ && sudo chmod +x /usr/local/bin/keepass4brute
+
+# Download zip2hashcat
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/zip2hashcat && sudo mv zip2hashcat /usr/local/bin/ && sudo chmod +x /usr/local/bin/zip2hashcat
+
+# Download addusertogroup.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/addusertogroup.py && sudo mv addusertogroup.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/addusertogroup.py
+
+# Download firepwd.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/firepwd.py && sudo mv firepwd.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/firepwd.py
+
+# Download firefox_decrypt.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/firefox_decrypt.py && sudo mv firefox_decrypt.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/firefox_decrypt.py
+
+# Download gitdumper.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/gitdumper.py && sudo mv gitdumper.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/gitdumper.py
+
+# Download targetedKerberoast.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/targetedKerberoast.py && sudo mv targetedKerberoast.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/targetedKerberoast.py
+
+# Download windapsearch.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/windapsearch.py && sudo mv windapsearch.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/windapsearch.py
+
+# Download smbpasswd.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/smbpasswd.py && sudo mv smbpasswd.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/smbpasswd.py
+
+# Download sliver.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/sliver.py && sudo mv sliver.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/sliver.py
+
+# Download laps_dumper.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/laps_dumper.py && sudo mv laps_dumper.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/laps_dumper.py
+
+# Download shellcoderunneraes.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/shellcoderunneraes.py && sudo mv shellcoderunneraes.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/shellcoderunneraes.py
+
+# Download ntlm_theft.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/ntlm_theft.py && sudo mv ntlm_theft.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/ntlm_theft.py
+
+# Download gMSADumper.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/gMSADumper.py && sudo mv gMSADumper.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/gMSADumper.py
+
+# Download keytabextract.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/keytabextract.py && sudo mv keytabextract.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/keytabextract.py
+
+# Download ReconSpider.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/ReconSpider.py && sudo mv ReconSpider.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/ReconSpider.py
+
+# Download iis_shortname_scan.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/iis_shortname_scan.py && sudo mv iis_shortname_scan.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/iis_shortname_scan.py
+
+# Download GPOwned.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/GPOwned.py && sudo mv GPOwned.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/GPOwned.py
+
+# Download ntlm_passwordspray.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/ntlm_passwordspray.py && sudo mv ntlm_passwordspray.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/ntlm_passwordspray.py
+
+# Download sid_converter.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/sid_converter.py && sudo mv sid_converter.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/sid_converter.py
+
+# Download tgsrepcrack.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/tgsrepcrack.py && sudo mv tgsrepcrack.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/tgsrepcrack.py
+
+# Download evil_winrmexec.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/evil_winrmexec.py && sudo mv evil_winrmexec.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/evil_winrmexec.py
+
+# Download winrmexec.py
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/winrmexec.py && sudo mv winrmexec.py /usr/local/bin/ && sudo chmod +x /usr/local/bin/winrmexec.py
+
+# Download kerbrute
+wget https://github.com/boboaung1337/adtools/raw/refs/heads/main/kerbrute && sudo mv kerbrute /usr/local/bin/ && sudo chmod +x /usr/local/bin/kerbrute
+
 echo "All tools installed successfully!"
+
